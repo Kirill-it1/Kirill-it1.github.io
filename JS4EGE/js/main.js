a56b6e11f6c6d4b453f85775bd7f30322e6fb44b
Kirill-it1 <90218602+Kirill-it1@users.noreply.github.com>
2022-11-18 13:01:40 +0300

    update

--- JS4EGE/js/main.js
+++ JS4EGE/js/main.js
@@ -1,9 +1,10 @@
-const input_txt = document.querySelector('#file-txt');
-const input_js = document.querySelector('#file-js');
+const input_txt = document.querySelector('#file-txt'),
+	input_js = document.querySelector('#file-js'),
+	myCons = document.querySelector('#console');
 
-const myCons = document.querySelector('#console');
 let myConsole = {};
-	myConsole.in = myCons.innerHTML;
+
+myConsole.in = myCons.innerHTML;
 
 myConsole.scroll = () => {
 
@@ -30,117 +31,113 @@ const btn_text_js = document.querySelector('#text_js');
 
 
 
-	console.stdlog = console.log.bind(console);
-	console.logs = [];
+console.stdlog = console.log.bind(console);
+console.logs = [];
 
-	console.log = function(){
-		console.logs.push(Array.from(arguments));
-		console.stdlog.apply(console, arguments);
-		myConsole.write(Array.from(arguments), false); 
+console.log = function(){
+	console.logs.push(Array.from(arguments));
+	console.stdlog.apply(console, arguments);
+	myConsole.write(Array.from(arguments), false); 
+}
+console.stdlog(console.logs);
+
+let files,
+	text_file = false,
+	js_file = false,
+	fileText = '',
+	fileJs = '';
+
+document.addEventListener('dragover', ev => ev.preventDefault());
+document.addEventListener('drop', ev => ev.preventDefault());
+
+
+
+const main = () =>{
+	
+	if(js_file === true && text_file === true){
+		output = '';
+		input = fileText;
+		eval(fileJs);
+		out = output.toString();
+		if(out.length > 0)
+			myConsole.write(out, true);
 	}
-	console.stdlog(console.logs);
 
-	let files;
+}
 
-		let text_file = false;
-		let js_file = false;
-		let fileText = '';
-		let fileJs = '';
 
-	document.addEventListener('dragover', ev => ev.preventDefault());
-	document.addEventListener('drop', ev => ev.preventDefault());
-
+// обрабатываем изменение инпута
+input_txt.addEventListener('change', () => {
+	// извлекаем File
+	file = input_txt.files[0]
+	
+	// передаем файл в функцию для дальнейшей обработки
 	
 
-	const main = () =>{
-		
-		if(js_file === true && text_file === true){
-			output = '';
-			input = fileText;
-			eval(fileJs);
-			out = output.toString();
-			if(out.length > 0)
-				myConsole.write(out, true);
-		}
-
+	const handleFile = (file) => {
+	if(file.type !== 'text/plain'){
+		btn_text.innerHTML = `<span class="red">Неподходящий файл</span>`;
+		text_file = false;
+		return;
 	}
+	btn_text.innerHTML = `<span class="blue">${file.name}</span>`;
+
+	createText(file)
+}
+		const createText = text => {
+		// создаем экземпляр объекта "FileReader"
+		const reader = new FileReader()
+		// читаем файл как текст
+		// вторым аргументом является кодировка
+		// по умолчанию - utf-8,
+		// но она не понимает кириллицу
+		reader.readAsText(text, 'windows-1251')
+		// дожидаемся завершения чтения файла
+		// и помещаем результат в документ
+		reader.onload = () => {
+
+			text_file = true;
+			fileText = reader.result;
+			main();
+	}
+}
+handleFile(file);
+
+});
 
 
-		// обрабатываем изменение инпута
-		input_txt.addEventListener('change', () => {
-				// смотрим на то, что получаем
-				// извлекаем File
-				file = input_txt.files[0]
-				
-				// передаем файл в функцию для дальнейшей обработки
-				
-
-				const handleFile = (file) => {
-			if(file.type !== 'text/plain'){
-				btn_text.innerHTML = `<span class="red">Неподходящий файл</span>`;
-				text_file = false;
-				return;
-			}
-			btn_text.innerHTML = `<span class="blue">${file.name}</span>`;
-
-			createText(file)
+input_js.addEventListener('change', () => {
+	// извлекаем File
+	file = input_js.files[0]
+	
+	// передаем файл в функцию для дальнейшей обработки
+	
+	const handleFile = (file) => {
+		if(file.type !== 'text/javascript'){
+			btn_text_js.innerHTML = `<span class="red">Неподходящий файл</span>`;
+			js_file = false;
+			return;
 		}
-				const createText = text => {
-				// создаем экземпляр объекта "FileReader"
-				const reader = new FileReader()
-				// читаем файл как текст
-				// вторым аргументом является кодировка
-				// по умолчанию - utf-8,
-				// но она не понимает кириллицу
-				reader.readAsText(text, 'windows-1251')
-				// дожидаемся завершения чтения файла
-				// и помещаем результат в документ
-				reader.onload = () => {
+		btn_text_js.innerHTML = `<span class="blue">${file.name}</span>`;
 
-					text_file = true;
-					fileText = reader.result;
-					main();
-			}
+		createText(file)
+	}
+	const createText = text => {
+		// создаем экземпляр объекта "FileReader"
+		const reader = new FileReader()
+		// читаем файл как текст
+		// вторым аргументом является кодировка
+		// по умолчанию - utf-8,
+		// но она не понимает кириллицу
+		reader.readAsText(text, 'windows-1251')
+		// дожидаемся завершения чтения файла
+		// и помещаем результат в документ
+		reader.onload = () => {
+			js_file = true;
+			fileJs = reader.result;
+			main();
 		}
-		handleFile(file);
+	}
+	handleFile(file);
 
-		});
-
-
-		input_js.addEventListener('change', () => {
-				// смотрим на то, что получаем
-				// извлекаем File
-				file = input_js.files[0]
-				
-				// передаем файл в функцию для дальнейшей обработки
-				
-
-				const handleFile = (file) => {
-						if(file.type !== 'text/javascript'){
-								btn_text_js.innerHTML = `<span class="red">Неподходящий файл</span>`;
-								js_file = false;
-								return;
-						}
-						btn_text_js.innerHTML = `<span class="blue">${file.name}</span>`;
-
-						createText(file)
-				}
-				const createText = text => {
-						// создаем экземпляр объекта "FileReader"
-						const reader = new FileReader()
-						// читаем файл как текст
-						// вторым аргументом является кодировка
-						// по умолчанию - utf-8,
-						// но она не понимает кириллицу
-						reader.readAsText(text, 'windows-1251')
-						// дожидаемся завершения чтения файла
-						// и помещаем результат в документ
-						reader.onload = () => {
-								js_file = true;
-								fileJs = reader.result;
-								main();
-						}
-				}
-				handleFile(file);
-
-		});
+});
