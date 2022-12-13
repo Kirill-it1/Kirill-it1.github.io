3addc1a4b004a4cacd11af646c035531bf54a45f
Kirill-it1 <90218602+Kirill-it1@users.noreply.github.com>
2022-12-13 16:34:24 +0300

    update

---
+++ js/main.js
@@ -0,0 +1,66 @@
+const links = document.querySelectorAll('.header-nav__link');
+const CLASS_ACTIVE_LINK = 'header-nav__link_active';
+
+const burger = document.querySelector('.header__burger');
+const menu = document.querySelector('.header__menu');
+const header = document.querySelector('.header');
+
+
+
+
+links.forEach((link)=>{
+
+  link.addEventListener('click', (e) => {
+  	e.preventDefault();
+    let hash = link.href.replace(/[^#]*(.*)/, '$1');
+    
+    location.hash = hash;
+    active_link(hash);
+    hideMenu();
+    
+  }, false);
+});
+
+let active_link = (hr) => {
+	let something = false;
+	links.forEach((link) => {
+		let hash = link.href.replace(/[^#]*(.*)/, '$1');
+		
+		if(hash == hr){
+			link.classList.add(CLASS_ACTIVE_LINK);
+			something = true;
+		} else {
+			link.classList.remove(CLASS_ACTIVE_LINK);
+		}
+	});
+}	
+
+active_link(location.hash);
+
+menu.addEventListener('click', () => {
+	hideMenu();
+});
+
+let hideMenu = () => {
+	menu.classList.remove('header__menu_active');
+	header.classList.remove('header_active');
+	burger.classList.remove('header__burger_active');
+	document.querySelector('body').style.overflow = '';
+}
+
+
+
+burger.addEventListener('click', () => {
+	menu.classList.toggle('header__menu_active');
+	header.classList.toggle('header_active');
+	flag = burger.classList.toggle('header__burger_active');
+	if(flag) {
+		document.querySelector('body').style.overflow = 'hidden';
+	} else {
+		document.querySelector('body').style.overflow = '';
+	}
+});
+
+
+
+
