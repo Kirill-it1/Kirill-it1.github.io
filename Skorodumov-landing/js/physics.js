1dc25a2b9106c63d520b3f00a5e360071e30d5a5
Kirill-it1 <90218602+Kirill-it1@users.noreply.github.com>
2023-09-11 22:17:56 +0300

    it`s the landing for Boris Scorodumov

---
+++ Skorodumov-landing/js/physics.js
@@ -0,0 +1,37 @@
+let stars = document.querySelectorAll('.physics__star'),
+	descs = document.querySelectorAll('.physics__desc');
+
+stars.forEach((star, index) => {
+
+	star.addEventListener('click', () => {
+		stars.forEach((el) => {el.classList.remove('physics__star_active')});
+		star.classList.add('physics__star_active');
+
+		descs.forEach((el) => {el.classList.remove('physics__desc_active')});
+		switch(index) {
+			case 0: 
+				descs[0].classList.add('physics__desc_active');
+				break;
+			case 1: 
+				descs[1].classList.add('physics__desc_active');
+				break;
+			case 2: 
+			case 3: 
+				stars[2].classList.add('physics__star_active');
+				stars[3].classList.add('physics__star_active');
+				descs[2].classList.add('physics__desc_active');
+				break;
+			case 4: 
+			case 5: 
+				stars[4].classList.add('physics__star_active');
+				stars[5].classList.add('physics__star_active');
+				descs[3].classList.add('physics__desc_active');
+				break;
+			case 6:
+				stars[6].classList.add('physics__star_active');
+				descs[4].classList.add('physics__desc_active');
+				break;
+				
+		}
+	});
+});
