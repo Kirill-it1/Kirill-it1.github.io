f65a9eb0ac6189bdbd0340d410812f1a1872db93
Kirill-it1 <90218602+Kirill-it1@users.noreply.github.com>
2023-11-02 18:28:48 +0300

    upd

---
+++ js/contact.js
@@ -0,0 +1,14 @@
+const moveToStart = (input) => {
+  input.setSelectionRange(0,0);
+  input.focus();
+}
+
+const select = document.querySelector('#country-select');
+const choices = new Choices(select, {
+	itemSelectText: '',
+});
+const selectedCountryFlag = document.querySelector('.contact-block__flag');
+select.addEventListener('addItem', (event) => {
+	// console.log(event.detail.value)
+	selectedCountryFlag.style.backgroundImage = `url(../img/icons/flags/${event.detail.value}.png)`;
+}, false);
