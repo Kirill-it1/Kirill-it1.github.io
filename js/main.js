5394d154e12ef84296e848181072f3a870bd8a83
Kirill-it1 <90218602+Kirill-it1@users.noreply.github.com>
2023-11-03 20:38:16 +0300

    update

--- js/main.js
+++ js/main.js
@@ -101,9 +101,3 @@ serv_card.forEach((el, i)=>{
     serv_pag_bul[i].classList.remove('swiper-pagination-bullet_f');
   });
 });
-const cen_h = () => {
-  document.querySelector('.planetarium-center').classList.toggle('planetarium-center_h');
-}
-document.querySelector('.planetarium-center__button_top').addEventListener('mouseover', cen_h);
-document.querySelector('.planetarium-center__button_top').addEventListener('mouseover', cen_h);
-
