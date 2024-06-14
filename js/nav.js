364f6a61f75af1bb10fbdb8407708a27bffb204e
Kirill-it1 <90218602+Kirill-it1@users.noreply.github.com>
2024-06-14 19:02:51 +0300

    hide nav

--- js/nav.js
+++ js/nav.js
@@ -4,14 +4,28 @@ const header = document.querySelector('.header');
 const upper = document.querySelector('.upper');
 
 let activeNav = false
-burger.addEventListener('click', () => { 
-  burger.classList.toggle('header-burger_active');
+const hid_nav = () => {
+	burger.classList.toggle('header-burger_active');
   nav.classList.toggle('header__nav_active');
   header.classList.toggle('header_active');
   upper.classList.toggle('upper_active');
   document.querySelector('body').classList.toggle('body_noscroll')
+	if (activeNav) {
+		activeNav = false
+	}
+	else	{
+		 activeNav = true
+	}
 
 
+}
 
 
+document.addEventListener('click', (e) => {
+	console.log(activeNav)
+	if (activeNav && !e.target.closest('.header'))
+		hid_nav()
+});
+burger.addEventListener('click', () => { 
+  hid_nav()
 });
